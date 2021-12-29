c5f3eb8ae99146fc08130d8cdef2dc0256ac2336
Hasan Ali Bağıran <85124037+hasanalibagiran@users.noreply.github.com>
2021-12-29 18:24:42 +0300

    Add files via upload

---
+++ downloader.py
@@ -0,0 +1,21 @@
+import youtube_dl
+
+def download():
+    video_url = input("Enter youtube url:")
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url= video_url,
+        download=False
+        )
+    filename = f"{video_info['title']}.mp3"
+    options = {
+        'format' : 'bestaudio/best',
+        'keepvideo' : False,
+        'outtmp1' : filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl :
+        ydl.download([video_info['webpage_url']])
+    print("{} download process has started ".format(video_info['title']))
+
+if __name__ == '__main__':
+    download()
